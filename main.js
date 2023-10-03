faa111a792929b933541fb78e62ea056d47e5802
AgiotaAngolano <105888321+AgiotaAngolano@users.noreply.github.com>
2023-10-03 08:11:21 -0300

    ,

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 function tocar(idElementoAudio) {
-    document.querySelector(idElementAudio).play();
+    document.querySelector(`#som_tecla_pom`).play();
 }
 
-const lista = document.querySelectorAll(`´.tecla`);
+const lista = document.querySelectorAll(`.tecla`);
 
 let contador = 0;
 
-const = lista[5].classList[1];
+while (contador < lista.length)
+    const tecla = lista[Contador];
+    const classe = tecla.classList[1];
+    //console.log(classe);
 
-while (contador < lista.length) { 
-    lista[contador].onclick = tocar();
-    contador = contador + 1;
-    console.log(contador);
-}
-.
+    const idSom = `´#som_${classe}`;
+    //console.log(idSom);
+
+    tecla.onclick = function(){
+        tocar(idSom);
+    }
+
+    contador = Contador + 1;
+    //console.log(Contador);
+    
+    tecla.onkeydown = function (evento){
+    if(evento.code === ´Space` || evento.code === `Enter´);
+      tecla.classList.add(´ativa´);
+    //console.log(evento.code === `Space´) || evento.code === `Enter´);
+
+    }
+    tecla.onkeyup = function(){
+tecla.classList.remove(´ativa´)
+    }
