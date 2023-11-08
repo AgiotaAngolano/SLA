72828155a8c881ec0463dc7fe9c0aed49537adf3
AgiotaAngolano <105888321+AgiotaAngolano@users.noreply.github.com>
2023-11-08 16:27:33 -0300

    .

--- main.js
+++ main.js
@@ -1,32 +1,4 @@
-
-
-let contador = 0;
-
-while (contador < lista.length)
-    const tecla = lista[Contador];
-    const classe = tecla.classList[1];
-    //console.log(classe);
-
-    const idSom = '#som_${classe}';
-    //console.log(idSom);
-
-    tecla.onclick = function(){
-        tocar(idSom);
-    }
-
-    contador = Contador + 1;
-    //console.log(Contador);
-    
-    tecla.onkeydown = function (evento){
-    if(evento.code === 'Space'|| evento.code === 'Enter');
-      tecla.classList.add('ativa');
-    //console.log(evento.code === 'Space') || evento.code === 'Enter');
-
-    }
-    tecla.onkeyup = function(){
-tecla.classList.remove('ativa')
-    }
-    function texto(){
+function texto(){
         //elemento pego do id//
         var num = document.getElementById("num");
         var num2 = document.getElementById("num2");
